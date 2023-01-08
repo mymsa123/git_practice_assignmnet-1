558fc8baf477550131036bc7b955c777b55309b6
mymsa123 <sabir70779560@gmail.com>
2023-01-08 22:38:35 +0530

    Palindrome is done

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let str = "naman"
+ let reverse = ""
+    for(let i = N-1 ; i>=0 ; i--){
+        reverse = reverse + str[i] +""
+        }
+   
+    if(reverse == str){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
